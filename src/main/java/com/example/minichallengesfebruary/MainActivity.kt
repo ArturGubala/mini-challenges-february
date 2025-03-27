841c7eea0bd142b058baea392d44144d73fa4ca0
arturgie <gubala-artur@wp.pl>
2025-03-27 20:12:30 +0100

    Add color do BatteryIndicator challenge Refactor MainActivity class.

--- src/main/java/com/example/minichallengesfebruary/MainActivity.kt
+++ src/main/java/com/example/minichallengesfebruary/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.minichallengesfebruary
 
-import OptionPicker
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.minichallengesfebruary.ui.theme.MiniChallengesFebruaryTheme
 
 class MainActivity : ComponentActivity() {
--- src/main/java/com/example/minichallengesfebruary/ui/theme/Color.kt
+++ src/main/java/com/example/minichallengesfebruary/ui/theme/Color.kt
@@ -2,14 +2,15 @@ package com.example.minichallengesfebruary.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Option picker
 val PickerBackground = Color(0x148138FF)
 val PickerInactiveText = Color(0xFF24005A)
+
+// Battery indicator
+val Surface = Color(0xFFE7E9EF)
+val SurfaceLow = Color(0xFFE7E9EF)
+val SurfaceHeight = Color(0xFFFFFFFF)
+val Red = Color(0xFFFF4E51)
+val RedAlt = Color(0xFFEF1242)
+val Green = Color(0xFFFCB966)
+val Yellow = Color(0xFF19D181)
